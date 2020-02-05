0da3b0a13e4e4eaebd93ec4cbd9794ad1aa48d78
Rasmus Kaj <kaj@kth.se>
2020-02-05 21:24:28 +0100

    Nicer output, cmd-line args.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 mod tone;
 
+use std::env::args;
+use std::process::exit;
 use tone::{Key, Tone};
 
 fn main() {
-    let strings: Vec<Tone> = ["G", "D", "A", "E"] // Mandolin
-        // ["E", "A", "D", "G", "B", "E"] // Guitar
+    let mandolin: Vec<Tone> = ["G", "D", "A", "E"]
+        .iter()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let guitar: Vec<Tone> = ["E", "A", "D", "G", "B", "E"]
         .iter()
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let key_s = "C#m";
-    let key = key_s.parse::<Key>().unwrap();
-
     println!(
-        "<style>\
+        "<!doctype html>\
+         \n<meta charset='utf-8'>\
+         \n<style>\
+         \ndiv.chord {{ display: inline-block; padding: 1em; }}\
+         \ncaption {{ font: italic bold 1.2em serif }}\
          \ntd {{\
          \n  border: 1px solid lightgray;\
          \n  width: 2em;\
@@ -23,11 +29,10 @@ fn main() {
          \n}}\
          \ntr:nth-child(1) td {{\
          \n  border: 0;\
+         \n  border-bottom: double 4px black;\
          \n}}\
          \nth {{\
-         \n  font-size: 80%;\
-         \n  font-weight: normal;\
-         \n  font-style: italic;\
+         \n  padding-right: .2em;\
          \n}}\
          \ntd {{\
          \n  text-align: center;\
@@ -42,19 +47,44 @@ fn main() {
         );
     }
     println!("</style>");
-    println!("<table>");
-    println!("<caption>{}</caption>", key_s);
-    for fret in 0..=12 {
-        print!("<tr><th>{:2}:</th>", fret);
-        for string in &strings {
-            print!(
-                "<td>{}</td>",
-                key.harmonic(string + fret)
-                    .map(|i| i.to_string())
-                    .unwrap_or(" ".to_string())
-            )
+
+    let mut strings = &guitar;
+    for arg in args().skip(1) {
+        match arg.as_str() {
+            "mandolin" => { strings = &mandolin; }
+            "guitar" => { strings = &guitar; }
+            key_s => {
+                let key = match key_s.parse::<Key>() {
+                    Ok(key) => key,
+                    Err(e) => {
+                        eprintln!("Exepected instrument or chord, got {:?}: {}",
+                                  key_s, e);
+                        exit(1);
+                    }
+                };
+                println!("<div class='chord'><table>");
+                println!("<caption>{}</caption>", key_s);
+                for fret in 0..=12 {
+                    print!(
+                        "<tr><th>{}</th>",
+                        match fret {
+                            3|5|7|10 => '·',
+                            12 => ':',
+                            _ => ' ',
+                        }
+                    );
+                    for string in strings {
+                        print!(
+                            "<td>{}</td>",
+                            key.harmonic(string + fret)
+                                .map(|i| i.to_string())
+                                .unwrap_or(" ".to_string())
+                        )
+                    }
+                    println!("</tr>");
+                }
+                println!("</table></div>");
+            }
         }
-        println!("</tr>");
     }
-    println!("</table>");
 }
