ed3434254bcdf2c132f6e1e82482d6c78b3f67b3
Rasmus Kaj <kaj@kth.se>
2020-02-06 22:59:44 +0100

    Draw strings and frets, not cell borders.

--- src/main.rs
+++ src/main.rs
@@ -22,23 +22,38 @@ fn main() {
          \ndiv.chord {{ display: inline-block; padding: 1em; }}\
          \ncaption {{ font: italic bold 1.2em serif }}\
          \ntd {{\
-         \n  border: 1px solid lightgray;\
+         \n  border: 0 solid gray;\
+         \n  border-width: 1px 0;
+         \n  padding: 0;\
+         \n  text-align: center;\
          \n  width: 2em;\
          \n}}\
+         \n.guitar td {{\
+         \n  background-image: linear-gradient(90deg, transparent 46%, #fd6, #fd6, transparent 54%);\
+         \n}}\
+         \n.guitar td:nth-child(n + 5) {{\
+         \n  background-image: linear-gradient(90deg, transparent 48%, #fd6, transparent 52%);\
+         \n}}\
+         \n.mandolin td {{\
+         \n  background-image: linear-gradient(90deg, transparent 40%, #fd6, transparent, transparent, transparent, #fd6, transparent 60%);\
+         \n}}\
+         \n.mandolin td:nth-child(n + 4) {{\
+         \n  background-image: linear-gradient(90deg, transparent 41%, #fd6, transparent, transparent, transparent, transparent, #fd6, transparent 59%);\
+         \n}}\
+         \ntd:nth-child(2) {{ border-left: 1px solid gray }}\
+         \ntd:last-child {{ border-right: 1px solid gray }}\
          \ntable {{\
          \n  border-collapse: collapse;\
          \n}}\
-         \ntr:nth-child(1) td {{\
+         \n.chord tr:nth-child(1) td {{\
+         \n  background-image: none;\
          \n  border: 0;\
          \n  border-bottom: double 4px black;\
          \n}}\
          \nth {{\
          \n  padding-right: .2em;\
          \n}}\
-         \ntd {{\
-         \n  text-align: center;\
-         \n  padding: 0;\
-         \n}}"
+         "
     );
     for i in 2..=13 {
         println!(
@@ -50,13 +65,16 @@ fn main() {
     println!("</style>");
 
     let mut strings = &guitar;
+    let mut instrument = "guitar";
     for arg in args().skip(1) {
         match arg.as_str() {
             "mandolin" => {
                 strings = &mandolin;
+                instrument = "mandolin";
             }
             "guitar" => {
                 strings = &guitar;
+                instrument = "guitar";
             }
             key_s => {
                 let key = match key_s.parse::<Key>() {
@@ -66,7 +84,7 @@ fn main() {
                         exit(1);
                     }
                 };
-                println!("<div class='chord'><table>");
+                println!("<div class='chord {}'><table>", instrument);
                 println!("<caption>{}</caption>", key_s);
                 for fret in 0..=12 {
                     print!(
@@ -84,7 +102,7 @@ fn main() {
                             "<td>{}</td>",
                             key.harmonic(string + fret)
                                 .map(|i| i.to_string())
-                                .unwrap_or(" ".to_string())
+                                .unwrap_or("".to_string())
                         )
                     }
                     println!("</tr>");
